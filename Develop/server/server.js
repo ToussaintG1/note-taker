81c429e98353a8f8b683a8bad9f87c63913dbd00
Toussaint Gilbert <toussaintdgilbert@gmail.com>
2023-08-21 18:10:46 -0500

    added express starter code

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -8,11 +8,45 @@ const app = express();
 const port = 3001;
 
 // tell the app to send the contents of the public folder 
+app.use(express.static(path.join(__dirname, '../public'))); 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  })
+
+  
+ app.get('/api/notes', (req, res) => {
+  // reading contents of the db.json file
+
+  // use fs to read the content of db.json
+
+
+  res.send();
 })
+
+// POST
+app.post('/api/notes', (req, res) => {
+    // reading contents of the db.json file
+  
+    // add new 
+
+    // write new content of file
+// "If" statement i
+    // Send message of "success" if it saves o
+  
+    res.status(200);
+  })
+
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
